521f1fd283c6a32399f5844d4ea393a702613894
thaibalong7 <thai.balong.7@gmail.com>
2018-10-20 10:31:07 +0700

    +! add button basket on navigation bar and change app icon

--- food_delivery/src/navigators/rootstack.js
+++ food_delivery/src/navigators/rootstack.js
@@ -2,8 +2,33 @@ import Login from '../components/presentationals/Login'
 import Register from '../components/presentationals/Register'
 import ForgetPassword from '../components/presentationals/ForgetPassword';
 import Merchant from '../components/containers/MerchantContainer';
+import Basket from '../components/presentationals/Basket'
 import { createStackNavigator } from 'react-navigation';
 import TabBar from './tabbar';
+import React from 'react';
+import {
+    TouchableOpacity, Text, View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import IconBadge from 'react-native-icon-badge';
+const basketButton = <TouchableOpacity>
+    <IconBadge MainElement={
+        <Icon style={{ marginRight: 20 }} name={'shopping-cart'} size={32} color={'white'}></Icon>
+    }
+        BadgeElement={
+            <Text style={{ color: '#FFFFFF', fontSize: 10 }}>{12}</Text>
+        }
+        IconBadgeStyle={
+            {
+                width: 12,
+                height: 12,
+                backgroundColor: '#DD2C00'
+            }
+        }
+        Hidden={false}
+    />
+</TouchableOpacity>;
+
 const RootStack = createStackNavigator({
     Login: {
         screen: Login,
@@ -29,7 +54,9 @@ const RootStack = createStackNavigator({
             title: 'Merchant',
             headerStyle: {
                 backgroundColor: '#FF9800',
+                height: 45
             },
+            headerRight: basketButton,
             headerTintColor: '#fff',
             headerTitleStyle: {
                 fontWeight: 'bold',
@@ -41,12 +68,17 @@ const RootStack = createStackNavigator({
         navigationOptions: {
             headerStyle: {
                 backgroundColor: '#FF9800',
+                height: 45
             },
+            headerRight: basketButton,
             headerTintColor: '#fff',
             headerTitleStyle: {
                 fontWeight: 'bold',
             },
         },
+    },
+    Basket: {
+        screen: Basket,
     }
 },
     {
